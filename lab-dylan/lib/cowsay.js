3531dbe2c9bd62c9aa7d5f3f88e177ae3bf52f66
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-12 10:30:26 -0700

    pre pull commit

--- lab-dylan/lib/cowsay.js
+++ lab-dylan/lib/cowsay.js
@@ -1,29 +1,19 @@
 module.exports = function(cowsay) {
-  var cow = {};
-  var length = cowsay.length;
-  cow.top = ' _';
-  cow.bot = ' -';
-  for (var i = 0; i < length; i++) {
-    cow.top += '_';
-    cow.bot += '-';
-  }
-  cow.top += '_ ';
-  cow.says = '< ' + cowsay + ' >';
-  cow.bot += '- ';
-  cow.one = '         \\    ^__^';
-  cow.two = '          \\   (oo)\\_______';
-  cow.thr = '              (__) \\      )\\/\\';
-  cow.fou = '                   ||----W |';
-  cow.fiv = '                   ||     ||';
+  var header = ' _' + '_'.repeat(cowsay.length) + '_ ';
+  var botBorder = ' -' + '-'.repeat(cowsay.length) + '- ';
+  var says = '< ' + cowsay + ' >';
+  var theCow = '\n' +
+  '         \\    ^__^ \n' +
+  '           \\   (oo)\\_______ \n' +
+  '              (__) \\      )\\/\\ \n' +
+  '                   ||----W | \n' +
+  '                   ||     ||';
 
-  console.log(cow.top);
-  console.log(cow.says);
-  console.log(cow.bot);
-  console.log(cow.one);
-  console.log(cow.two);
-  console.log(cow.thr);
-  console.log(cow.fou);
-  console.log(cow.fiv);
+  var cow = '
+  |
+  |
+  |
+                      ';
   // cow.cowsays = cow.top + '\n' + cow.says + '\n' + cow.bot + '\n' + cow.one + '\n' + cow.two + '\n' + cow.thr + '\n' + cow.fou + '\n' + cow.fiv;
   // return cow;
 };
