84fe9d8f71ea73bcb821506ab988e56f16645a0e
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-12 10:30:48 -0700

    derp

--- lab-dylan/test/cowsay_test.js
+++ lab-dylan/test/cowsay_test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const cowsay = require('../lib/cowsay');
 describe('cow says', function(){
   it('should make the cow say', function() {
-
+    expect(cowsay('mooo').eql(''));
   });
 
 });
