a9a2e03be3c39e30d926f959002d8a63d7412fdf
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-12 15:17:02 -0700

    deleted unused bin file

--- lab-dylan/lib/cowsay_bin.js
+++
@@ -1,3 +0,0 @@
-// Terminal Utility Attempt
-// const cowsay = require('./cowsay');
-// console.log(cowsay(process.argv.splice(2).join(' ')));
--- lab-dylan/test/cowsay_test.js
+++ lab-dylan/test/cowsay_test.js
@@ -1,10 +1,9 @@
 const expect = require('chai').expect;
 const cowsay = require('../lib/cowsay');
+
 describe('cow says', function(){
   it('should make the cow say moo', function() {
-    var e = ' ___ \n< moo >\n --- \n         \\    ^__^ \n          \\   (oo)\\_______ \n              (__) \\      )\\/\\ \n                   ||----W | \n                   ||     ||';
-
-    expect(cowsay('moo')).to.equal(e);
+    var expected = ' ___ \n< moo >\n --- \n         \\    ^__^ \n          \\   (oo)\\_______ \n              (__) \\      )\\/\\ \n                   ||----W | \n                   ||     ||';
+    expect(cowsay('moo')).to.equal(expected);
   });
-
 });
