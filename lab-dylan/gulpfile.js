0e124aae25c706ac1bb95a2b63b9fae44977da79
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-13 12:55:38 -0700

    did the bonus portions

--- lab-dylan/gulpfile.js
+++ lab-dylan/gulpfile.js
@@ -2,19 +2,66 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
+var options = {
+  rules: {
+    'no-console': 0,
+    'indent': [
+      2,
+      2
+    ],
+    'quotes': [
+      2,
+      'single'
+    ],
+    'linebreak-style': [
+      2,
+      'unix'
+    ],
+    'semi': [
+      2,
+      'always'
+    ]
+  },
+  globals:  [
+    'describe',
+    'it',
+    'beforeEach',
+    'afterEach',
+    'before',
+    'after'
+  ],
+  envs: [
+    'es6',
+    'node',
+    'browser'
+  ],
+  ecmaFeatures: [
+    'modules',
+    'experimentalObjectRestSpread',
+    'impliedStrict'
+  ],
+  extends: 'eslint:recommended',
+  useEslintrc: false
+};
+
+
+var watcher = gulp.watch(['test/**/*.js', 'lib/**/*.js'], ['lint:app', 'lint:test', 'lint:gulp', 'mocha:app']);
+watcher.on('change', function() {
+  console.log('watcher test');
+});
 
 var testFiles = ['test/**/*.js'];
-var appFile = ['lib/**/*.js', 'bin/cowsay'];
+var appFile = ['lib/**/*.js'];
 var gulpFile = ['gulpfile.js'];
 gulp.task('lint:app', () => {
   gulp.src(appFile)
-    .pipe(eslint())
+      .pipe(eslint(options))
       .pipe(eslint.format());
 });
 
 gulp.task('lint:test', () => {
   gulp.src(testFiles)
-    .pipe(eslint())
+    .pipe(eslint(options))
     .pipe(eslint.format());
 });
 
@@ -25,8 +72,8 @@ gulp.task('mocha:app', () => {
 
 gulp.task('lint:gulp', () => {
   gulp.src(gulpFile)
-  .pipe(eslint())
-  .pipe(eslint());
+  .pipe(eslint(options))
+  .pipe(eslint.format());
 });
 
 gulp.task('default', ['lint:app', 'lint:test','lint:gulp', 'mocha:app']);
